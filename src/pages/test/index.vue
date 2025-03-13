004e3c183188c18540d8041de824dcf34acf6a67
jachin <407994173@qq.com>
2025-03-13 22:00:11 +0800

    fix error

--- src/pages/test/index.vue
+++ src/pages/test/index.vue
@@ -1,24 +1,25 @@
 <script lang='ts' setup>
-import { XMindEmbedViewer } from 'xmind-embed-viewer'
+// import { XMindEmbedViewer } from 'xmind-embed-viewer'
 
-onMounted(() => {
-  const viewer = new XMindEmbedViewer({
-    el: '#container', // HTMLElement | HTMLIFrameElement | string
-    // 如果在中国大陆境内速度慢，可以添加的参数 `region: 'cn'` 改为使用 xmind.cn 的图库作为依赖。
-    region: 'cn', // optinal, global(default) or cn
-  })
-  viewer.setStyles({
-    width: '100%',
-    height: '100%',
-  })
-  fetch('/xmind/1.xmind')
-    .then(res => res.arrayBuffer())
-    .then(file => viewer.load(file))
-})
+// onMounted(() => {
+//   const viewer = new XMindEmbedViewer({
+//     el: '#container', // HTMLElement | HTMLIFrameElement | string
+//     // 如果在中国大陆境内速度慢，可以添加的参数 `region: 'cn'` 改为使用 xmind.cn 的图库作为依赖。
+//     region: 'cn', // optinal, global(default) or cn
+//   })
+//   viewer.setStyles({
+//     width: '100%',
+//     height: '100%',
+//   })
+//   fetch('/xmind/1.xmind')
+//     .then(res => res.arrayBuffer())
+//     .then(file => viewer.load(file))
+// })
 </script>
 
 <template>
   <div id="container" />
+  123
 </template>
 
 <style scoped>
